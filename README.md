f791ee08893d738e8d0ee6244aae14bf0012b1e8
JackRyan1989 <jack.jackryan@gmail.com>
2019-10-15 11:14:30 -0400

    Movie search and band search done.

--- README.md
+++ README.md
@@ -1,2 +1,43 @@
-# liri-node-app
-LIRI is a Node-based application that will take user input and search several apis based on that input.
+### liri-node-app
+LIRI is a Node-based application that will take user input and search several apis based on that input. Got a song stuck in your head? Can't think of the voice-actor of Donkey in Shrek? Look no further! 
+
+### Packages required: ###
+- Axios
+- Dotenv
+- Inquirer
+- Moment
+- Spotify
+
+### Usage ###
+LIRI queries users regarding 3 topics. LIRI can do this via the command line, or via .txt file input.
+
+- Search topics:
+
+1. Upcoming concerts for a searched for artist via BandsInTown
+    a. LIRI returns the concert venue, location of venue and the date of the concert.
+2. Song search via Spotify
+    a. LIRI returns artists who have recorded a song of the title
+    b. The song's name
+    c. A preview link to the song
+    d. The album that the song is from
+3. Movie title Search
+    a. LIRI returns:
+        * Title of the movie.
+        * Year the movie came out.
+        * IMDB Rating of the movie.
+        * Rotten Tomatoes Rating of the movie.
+        * Country where the movie was produced.
+        * Language of the movie.
+        * Plot of the movie.
+        * Actors in the movie.
+
+Command Line:
+
+File Input: 
+
+
+
+### Author ###
+Jack Ryan
+Github: jackryan1989
+Email: jack.jackryan@gmail.com
--- liri.js
+++ liri.js
@@ -1,3 +1,5 @@
+//Require the filesystem so we can read and write files:
+var fs = require('fs');
 
 //Require dotenv so that we can pull in the API keys specific to my user.
 require('dotenv').config();
@@ -9,26 +11,139 @@ var spotify = require('node-spotify-api');
 //Require the moment.js node package:
 var moment = require('moment');
 
-//Require inquirer so that we can have a nice user interface:
-var inquirer = require('inquirer');
-
 //Require axios so that we can get things from our apis of interest: 
 var axios = require('axios');
 
-//Use inquirer to acquire user input:
+//Require inquirer for a sensuous user interface: 
+var inquirer = require('inquirer');
 
-inquirer.prompt([
-{
-    type: 'input',
-    message: ''
-}
+var questions = [
+    {
+        type: "list",
+        message: 'What would you like to search for today?',
+        choices: ['Movie Info', 'Fav Band Concerts', 'Song Search', 'File Input'],
+        default: 'Movie Info',
+        name: 'whatDo'
+    },
+    {
+        type: 'input',
+        message: 'Please enter a Movie Title:',
+        name: 'movieTitle',
+        when: function (answers) {
+            if (answers.whatDo === 'Movie Info') {
+                return answers.whatDo;
+            }
+        }
+    },
+    {
+        type: 'input',
+        message: 'Please enter a Band:',
+        name: 'concert',
+        when: function (answers) {
+            if (answers.whatDo === 'Fav Band Concerts') {
+                console.log(answers.whatDo);
+                return answers.whatDo;
+            }
+        }
+    },
+    {
+        type: 'input',
+        message: 'Please enter a Song:',
+        name: 'song',
+        when: function (answers) {
+            if (answers.whatDo === 'Song Search') {
+                console.log(answers.whatDo);
+                return answers.whatDo;
+            }
+        }
+    },
+    {
+        type: 'input',
+        message: 'Please enter the File Name:',
+        name: 'fileName',
+        when: function (answers) {
+            if (answers.whatDo === 'File Input') {
+                console.log(answers.whatDo);
+                return answers.whatDo;
+            }
+        }
+    }
+];
 
-
-]).then(function(answers){
-
+inquirer.prompt(questions).then(function (answers) {
+    var toDo = answers.whatDo;
+    switch (toDo) {
+        case 'Movie Info':
+            axios.get("http://www.omdbapi.com/?t=" + answers.movieTitle + "&y=&plot=short&apikey=42a11145").then(
+                function (response) {
+                    if (response.data.Title === undefined) {
+                        return console.log("Movie not found. Search again.");
+                    } else {
+                        console.log("---------------Title---------------");
+                        console.log(response.data.Title);
+                        console.log("---------------Release Year---------------");
+                        console.log(response.data.Year);
+                        console.log("---------------IMDB Rating---------------");
+                        console.log(response.data.imdbRating);
+                        console.log("---------------Rotten Tomatoes Tomatometer---------------");
+                        console.log(response.data.Ratings[1].Value);
+                        console.log("---------------Country of Production---------------");
+                        console.log(response.data.Country);
+                        console.log("---------------Language---------------");
+                        console.log(response.data.Language);
+                        console.log("---------------Plot---------------");
+                        console.log(response.data.Plot);
+                        console.log("---------------Cast---------------");
+                        console.log(response.data.Actors);
+                    }
+                })
+                .catch(function (error) {
+                    if (error.response) {
+                        // The request was made and the server responded with a status code
+                        // that falls out of the range of 2xx
+                        console.log("---------------Data---------------");
+                        console.log(error.response.data);
+                        console.log("---------------Status---------------");
+                        console.log(error.response.status);
+                        console.log("---------------Status---------------");
+                        console.log(error.response.headers);
+                    }
+                });
+            break;
+        case 'Fav Band Concerts':
+            var artist = answers.concert;
+            axios.get("https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp").then(
+                function (response) {
+                    console.log(response.data[1]);
+                    for (i = 0; i < response.data.length; i++) {
+                    console.log("---------------Venue---------------");
+                    console.log(response.data[i].venue.name);
+                    console.log("---------------Location---------------");
+                    console.log(response.data[i].venue.city + ", " + response.data[i].venue.region + ", " + response.data[i].venue.country);
+                    console.log("---------------Date---------------");
+                    console.log(response.data[i].datetime);
+                    }
+                }).catch(function (error) {
+                    if (error.response) {
+                        // The request was made and the server responded with a status code
+                        // that falls out of the range of 2xx
+                        console.log("---------------Data---------------");
+                        console.log(error.response.data);
+                        console.log("---------------Status---------------");
+                        console.log(error.response.status);
+                        console.log("---------------Status---------------");
+                        console.log(error.response.headers);
+                    }
+                });
+            break;
+        case 'Song Search':
+            console.log("Song search");
+            break;
+        case 'File Input':
+            console.log("Search from file");
+            break;
+    }
 });
-
-
 
 
 
